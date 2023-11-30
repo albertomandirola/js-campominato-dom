2f471831fed59f75c310ee1ce2b7b22a85418206
albertomandirola <albymandi00@gmail.com>
2023-11-30 16:56:54 +0100

    [Update]added bombs

--- js/script.js
+++ js/script.js
@@ -1,3 +1,17 @@
+function arreyBomb(){
+    let bombs = []
+    let random_num;
+
+    while(bombs.length<16){
+        random_num = Math.floor(Math.random()*100 + 1)
+        if(!bombs.includes(random_num)){
+            bombs.push(random_num)
+        }
+    }
+    return bombs;
+
+}
+
 //funzione crea celle che dipende da due fattori
 function createCell(num ,row_num){
     const square = document.createElement('div')
@@ -12,12 +26,23 @@ function createCell(num ,row_num){
 function createNewGame(){
     let grid = document.getElementById('grid')
     grid.innerHTML = '';
+    let arrey_bombs = arreyBomb()
+    console.log(arrey_bombs)
 
     let row_num = 10; 
     for(let i=1; i<101; i++){
         let cell = createCell(i,row_num)
+        cell.addEventListener('click', function(){
+            if(arrey_bombs.includes(i)){
+                this.classList.add('bg-danger')
+            }
+            else{
+                this.classList.add('bg-white');
+            }
+        })
         grid.append(cell)
     }
+   
 }
 
 
