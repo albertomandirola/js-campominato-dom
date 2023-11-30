a0cfcc3582dcf26dc29ee5ea528f6d14029e70b0
albertomandirola <albymandi00@gmail.com>
2023-11-30 17:47:18 +0100

    [update] added point counter

--- js/script.js
+++ js/script.js
@@ -1,15 +1,16 @@
+//Funzione che mi permette di generare un arrey di numeri che saranno viste come bombe
 function arreyBomb(){
     let bombs = []
     let random_num;
-
+    //cicla fino a quando l'arrey non presenta 16 elementi
     while(bombs.length<16){
         random_num = Math.floor(Math.random()*100 + 1)
+        //if che mi permette di aggiungere elementi all arrey solo se non presenti
         if(!bombs.includes(random_num)){
             bombs.push(random_num)
         }
     }
     return bombs;
-
 }
 
 //funzione crea celle che dipende da due fattori
@@ -27,18 +28,34 @@ function createNewGame(){
     let grid = document.getElementById('grid')
     grid.innerHTML = '';
     let arrey_bombs = arreyBomb()
-    console.log(arrey_bombs)
-
-    let row_num = 10; 
+    let point = 0;
+    let row_num = 10;
+    let cell_tot = 100; 
+    console.log(arrey_bombs.length);
+    console.log(arrey_bombs);
+    
     for(let i=1; i<101; i++){
         let cell = createCell(i,row_num)
+        //FUNZIONE CLICK SULLA CELLA PER STABILIRE IL PUNTEGGIO 
         cell.addEventListener('click', function(){
-            if(arrey_bombs.includes(i)){
-                this.classList.add('bg-danger')
+            if(point == cell_tot - arrey_bombs.length){
+                alert('hai vinto')
             }
             else{
-                this.classList.add('bg-white');
+                if(arrey_bombs.includes(i)){
+                    this.classList.add('bg-danger')
+                    alert(`hai totalizzato un punteggio di ${point}. Clicca di nuovo il bottone per iniziare una nuova partita`)
+                    grid.innerHTML = '';
+                    document.getElementById('point').innerText = ``
+                }
+                else{
+                    
+                    this.classList.add('bg-white');
+                    point++
+                    document.getElementById('point').innerText = `${point}`
+                }
             }
+            
         })
         grid.append(cell)
     }
